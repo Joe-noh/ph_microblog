f4ae252cde287fd2c1e424a64847c1ff778e64e0
Joe-noh <goflb.jh@gmail.com>
2014-10-11 13:35:26 +0900

    add repo example

---
+++ lib/ph_microblog/repo.example.ex
@@ -0,0 +1,23 @@
+defmodule PhMicroblog.Repo do
+  use Ecto.Repo, adapter: Ecto.Adapters.Postgres, env: Mix.env
+
+  def conf(env) do
+    parse_url url(env)
+  end
+
+  def url(:dev) do
+    "ecto://postgres:password@localhost/ph_microblog_dev"
+  end
+
+  def url(:test) do
+    "ecto://postgres:password@localhost/ph_microblog_test?size=1"
+  end
+
+  def url(:prod) do
+    "ecto://postgres:password@localhost/ph_microblog"
+  end
+
+  def priv do
+    app_dir(:ph_microblog, "priv/repo")
+  end
+end
