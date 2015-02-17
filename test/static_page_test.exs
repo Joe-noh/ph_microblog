753d95f98a1fb94066d7343cd58985ebffc6785a
Joe-noh <goflb.jh@gmail.com>
2015-02-17 17:56:28 +0900

    add static_page test

---
+++ test/static_page_test.exs
@@ -0,0 +1,27 @@
+defmodule StaticPageTest do
+  use ExUnit.Case
+  use Plug.Test
+  import TestHelper
+
+  @opts PhMicroblog.Router.init []
+
+  test "home" do
+    conn = conn(:get, "/static_page/home", %{})
+      |> with_session
+      |> PhMicroblog.Router.call(@opts)
+
+    assert conn.state  == :sent
+    assert conn.status == 200
+    assert conn.resp_body =~ ~r|Home|
+  end
+
+  test "help" do
+    conn = conn(:get, "/static_page/help", %{})
+      |> with_session
+      |> PhMicroblog.Router.call(@opts)
+
+    assert conn.state  == :sent
+    assert conn.status == 200
+    assert conn.resp_body =~ ~r|Help|
+  end
+end
