f6269f5618fb5a8f56d3b1652de95446cf138bdb
Joe-noh <goflb.jh@gmail.com>
2016-05-24 06:37:28 +0900

    test the static pages return 200

--- test/controllers/static_page_controller_test.exs
+++ test/controllers/static_page_controller_test.exs
@@ -1,8 +1,21 @@
 defmodule PhMicroblog.StaticPageControllerTest do
   use PhMicroblog.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  test "GET home", %{conn: conn} do
+    conn = get conn, static_page_path(conn, :home)
+
+    assert html_response(conn, 200)
+  end
+
+  test "GET help", %{conn: conn} do
+    conn = get conn, static_page_path(conn, :help)
+
+    assert html_response(conn, 200)
+  end
+
+  test "GET about", %{conn: conn} do
+    conn = get conn, static_page_path(conn, :about)
+
+    assert html_response(conn, 200)
   end
 end
