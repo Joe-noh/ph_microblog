c0755f5b255e5d1f2c3febf0eb3d369c892f0dd8
Joe-noh <goflb.jh@gmail.com>
2016-05-28 13:59:18 +0900

    install comeonin

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule PhMicroblog.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {PhMicroblog, []},
@@ -32,17 +29,13 @@ defmodule PhMicroblog.Mixfile do
   defp apps do
     [
       :phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-      :phoenix_ecto, :postgrex
+      :phoenix_ecto, :postgrex, :comeonin
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.1.4"},
@@ -53,19 +46,17 @@ defmodule PhMicroblog.Mixfile do
       {:gettext, "~> 0.9"},
       {:cowboy, "~> 1.0"},
 
+      {:comeonin, "~> 2.4"},
+
       {:floki, "~> 0.8", only: :test},
       {:ex_machina, "~> 0.6.1", only: :test}
     ]
   end
 
-  # Aliases are shortcut or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+    ]
   end
 end
