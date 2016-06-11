cb09c22ff6a153be703b826cf36f5589010c06fd
Joe-noh <goflb.jh@gmail.com>
2016-06-11 19:01:04 +0900

    seed followers

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,4 +1,4 @@
-alias PhMicroblog.{User, Micropost, Repo}
+alias PhMicroblog.{User, Micropost, Relationship, Repo}
 
 import Ecto
 import Ecto.Query
@@ -18,7 +18,7 @@ end
 
 Faker.start
 
-Helper.insert_user!("Example User", "example@railstutorial.org", "foobar", true)
+example_user = Helper.insert_user!("Example User", "example@railstutorial.org", "foobar", true)
 
 1..99
 |> Enum.map(&Task.async(Helper, :insert_user!, [Faker.Name.name, "example-#{&1}@railstutorial.org", "password"]))
@@ -32,5 +32,25 @@ users = from(u in User, order_by: :inserted_at, limit: 6) |> Repo.all
     content = Faker.Lorem.sentence(5)
     user |> build_assoc(:microposts) |> Micropost.changeset(%{content: content}) |> Repo.insert!
   end
+end)
+|> Enum.map(&Task.await/1)
+
+from(u in User, offset: 2, limit: 48)
+|> Repo.all
+|> Enum.map(&Task.async fn ->
+  example_user
+  |> build_assoc(:active_relationships)
+  |> Relationship.changeset(%{followed_id: &1.id})
+  |> Repo.insert!
+end)
+|> Enum.map(&Task.await/1)
+
+from(u in User, offset: 3, limit: 37)
+|> Repo.all
+|> Enum.map(&Task.async fn ->
+  example_user
+  |> build_assoc(:passive_relationships)
+  |> Relationship.changeset(%{follower_id: &1.id})
+  |> Repo.insert!
 end)
 |> Enum.map(&Task.await/1)
