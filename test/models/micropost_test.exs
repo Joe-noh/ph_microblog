d64e0fe12666ac288d04c0744dfb2e82b69a4e9a
Joe-noh <goflb.jh@gmail.com>
2016-06-12 12:25:19 +0900

    use describe/2 in model tests

--- test/models/micropost_test.exs
+++ test/models/micropost_test.exs
@@ -9,30 +9,32 @@ defmodule PhMicroblog.MicropostTest do
     {:ok, [micropost: micropost]}
   end
 
-  test "content can't be blank", %{micropost: micropost} do
-    assert errors_on(micropost, %{content: ""})[:content]
-  end
+  describe "validations" do
+    test "content can't be blank", %{micropost: micropost} do
+      assert errors_on(micropost, %{content: ""})[:content]
+    end
 
-  test "content length can't be longer than 140", %{micropost: micropost} do
-    content = String.duplicate("a", 140)
-    refute errors_on(micropost, %{content: content})[:content]
+    test "content length can't be longer than 140", %{micropost: micropost} do
+      content = String.duplicate("a", 140)
+      refute errors_on(micropost, %{content: content})[:content]
 
-    content = String.duplicate("a", 141)
-    assert errors_on(micropost, %{content: content})[:content]
-  end
+      content = String.duplicate("a", 141)
+      assert errors_on(micropost, %{content: content})[:content]
+    end
 
-  test "user_id can't be blank" do
-    params = Factory.fields_for(:lorem)
+    test "user_id can't be blank" do
+      params = Factory.fields_for(:lorem)
 
-    assert errors_on(%Micropost{user_id: nil}, params)[:user_id]
-  end
+      assert errors_on(%Micropost{user_id: nil}, params)[:user_id]
+    end
 
-  test "user should exist" do
-    params = Factory.fields_for(:lorem)
-    changeset = %Micropost{user_id: -1} |> Micropost.changeset(params)
+    test "user should exist" do
+      params = Factory.fields_for(:lorem)
+      changeset = %Micropost{user_id: -1} |> Micropost.changeset(params)
 
-    assert_raise Ecto.InvalidChangesetError, fn ->
-      Repo.insert! changeset
+      assert_raise Ecto.InvalidChangesetError, fn ->
+        Repo.insert! changeset
+      end
     end
   end
 end
--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,102 +3,112 @@ defmodule PhMicroblog.UserTest do
 
   alias PhMicroblog.{Factory, User, Micropost}
 
-  setup do
-    user = Factory.build(:michael)
+  describe "validations" do
+    setup do
+      user = Factory.build(:michael)
 
-    {:ok, [user: user]}
-  end
+      {:ok, [user: user]}
+    end
 
-  test "name can't be blank", %{user: user} do
-    assert errors_on(user, %{name: ""})[:name]
-  end
+    test "name can't be blank", %{user: user} do
+      assert errors_on(user, %{name: ""})[:name]
+    end
 
-  test "name shouldn't be too long", %{user: user} do
-    name = String.duplicate("a", 50)
-    refute errors_on(user, %{name: name})[:name]
+    test "name shouldn't be too long", %{user: user} do
+      name = String.duplicate("a", 50)
+      refute errors_on(user, %{name: name})[:name]
 
-    name = String.duplicate("a", 51)
-    assert errors_on(user, %{name: name})[:name]
-  end
+      name = String.duplicate("a", 51)
+      assert errors_on(user, %{name: name})[:name]
+    end
 
-  test "email can't be blank", %{user: user} do
-    assert errors_on(user, %{email: ""})[:email]
-  end
+    test "email can't be blank", %{user: user} do
+      assert errors_on(user, %{email: ""})[:email]
+    end
 
-  test "email shouldn't be too long", %{user: user} do
-    email = String.duplicate("a", 243) <> "@example.com"
-    refute errors_on(user, %{email: email})[:email]
-
-    email = String.duplicate("a", 244) <> "@example.com"
-    assert errors_on(user, %{email: email})[:email]
-  end
-
-  test "email validation should accept valid addresses", %{user: user} do
-    ~w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-    |> Enum.each(fn email ->
+    test "email shouldn't be too long", %{user: user} do
+      email = String.duplicate("a", 243) <> "@example.com"
       refute errors_on(user, %{email: email})[:email]
-    end)
-  end
 
-  test "email validation should reject invalid addresses", %{user: user} do
-    ~w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
-    |> Enum.each(fn email ->
+      email = String.duplicate("a", 244) <> "@example.com"
       assert errors_on(user, %{email: email})[:email]
-    end)
+    end
+
+    test "email validation should accept valid addresses", %{user: user} do
+      ~w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      |> Enum.each(fn email ->
+        refute errors_on(user, %{email: email})[:email]
+      end)
+    end
+
+    test "email validation should reject invalid addresses", %{user: user} do
+      ~w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+      |> Enum.each(fn email ->
+        assert errors_on(user, %{email: email})[:email]
+      end)
+    end
+
+    test "email addresses should be unique", %{user: user} do
+      Factory.create(:michael, email: user.email)
+
+      assert {:error, _} = user |> User.changeset() |> Repo.insert()
+    end
+
+    test "email always will be saved in downcase", %{user: user} do
+      changeset = User.changeset(user, %{email: "HOGE@EXAMPLE.COM"})
+
+      assert changeset |> Ecto.Changeset.get_field(:email) == "hoge@example.com"
+    end
+
+    test "password shouldn't be too short", %{user: user} do
+      params = %{password: "aaaaa", password_confirmation: "aaaaa"}
+
+      assert errors_on(user, params)[:password]
+    end
+
+    test "password and its confirmation have to be same when there's password", %{user: user} do
+      params = %{password: "aaaaaa", password_confirmation: "bbbbbb"}
+
+      assert errors_on(user, params)[:password_confirmation]
+    end
+
+    test "update without password should succeed", %{user: user} do
+      user = Factory.create(:michael, email: user.email)
+
+      assert errors_on(user, %{name: "alex"}) == []
+    end
+
+    test "associated microposts should be destroyed", %{user: user} do
+      user = Repo.insert!(user)
+      micropost = Factory.create(:lorem, user: user)
+
+      Repo.delete(user)
+
+      assert Repo.get(Micropost, micropost.id) == nil
+    end
   end
 
-  test "email addresses should be unique", %{user: user} do
-    Factory.create(:michael, email: user.email)
+  describe "feed/1" do
+    setup do
+      michael = Factory.create(:michael)
+      archer  = Factory.create(:archer)
 
-    assert {:error, _} = user |> User.changeset() |> Repo.insert()
-  end
+      m_post = Factory.create(:lorem, user: michael)
+      a_post = Factory.create(:lorem, user: archer)
 
-  test "email always will be saved in downcase", %{user: user} do
-    changeset = User.changeset(user, %{email: "HOGE@EXAMPLE.COM"})
+      {:ok, [michael: michael, archer: archer, michael_post: m_post, archer_post: a_post]}
+    end
 
-    assert changeset |> Ecto.Changeset.get_field(:email) == "hoge@example.com"
-  end
+    test "following user's posts are included", context do
+      Factory.create(:relationship, follower: context.michael, followed: context.archer)
 
-  test "password shouldn't be too short", %{user: user} do
-    params = %{password: "aaaaa", password_confirmation: "aaaaa"}
+      refute context.michael_post in Repo.all(User.feed context.archer)
+      assert context.archer_post  in Repo.all(User.feed context.michael)
+    end
 
-    assert errors_on(user, params)[:password]
-  end
-
-  test "password and its confirmation have to be same when there's password", %{user: user} do
-    params = %{password: "aaaaaa", password_confirmation: "bbbbbb"}
-
-    assert errors_on(user, params)[:password_confirmation]
-  end
-
-  test "update without password should succeed", %{user: user} do
-    user = Factory.create(:michael, email: user.email)
-
-    assert errors_on(user, %{name: "alex"}) == []
-  end
-
-  test "associated microposts should be destroyed", %{user: user} do
-    user = Repo.insert!(user)
-    micropost = Factory.create(:lorem, user: user)
-
-    Repo.delete(user)
-
-    assert Repo.get(Micropost, micropost.id) == nil
-  end
-
-  test "feed/1" do
-    michael = Factory.create(:michael)
-    archer  = Factory.create(:archer)
-
-    michael_post = Factory.create(:lorem, user: michael)
-    archer_post  = Factory.create(:lorem, user: archer)
-
-    refute michael_post in Repo.all(User.feed archer)
-    refute archer_post  in Repo.all(User.feed michael)
-
-    Factory.create(:relationship, follower: michael, followed: archer)
-
-    refute michael_post in Repo.all(User.feed archer)
-    assert archer_post  in Repo.all(User.feed michael)
+    test "own posts are included", context do
+      assert context.michael_post in Repo.all(User.feed context.michael)
+      assert context.archer_post  in Repo.all(User.feed context.archer)
+    end
   end
 end
