ebde81a6566c6c58cf38164aef507196175606bc
Joe-noh <goflb.jh@gmail.com>
2016-06-25 02:31:43 +0900

    update packages

--- mix.exs
+++ mix.exs
@@ -39,11 +39,11 @@ defmodule PhMicroblog.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.2.0-rc"},
-      {:phoenix_pubsub, "~> 1.0.0-rc"},
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_ecto, "3.0.0-rc.0"},
-      {:phoenix_html, "~> 2.4"},
+      {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
       {:cowboy, "~> 1.0"},
