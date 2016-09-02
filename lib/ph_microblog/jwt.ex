573cff0c188d0f753acd3fdf85b0144210958686
Joe-noh <honzawa.j@pepabo.com>
2016-09-02 15:52:17 +0900

    secret key base should resolved on runtime

--- lib/ph_microblog/jwt.ex
+++ lib/ph_microblog/jwt.ex
@@ -1,13 +1,11 @@
 defmodule PhMicroblog.Jwt do
   import Joken
 
-  @secret Application.get_env(:ph_microblog, PhMicroblog.Endpoint)[:secret_key_base]
-
   @spec decode(String.t) :: {:ok, map} | {:error, String.t}
   def decode(jwt) do
     jwt
     |> token()
-    |> with_signer(hs256 @secret)
+    |> with_signer(hs256 secret)
     |> verify!()
   end
 
@@ -15,8 +13,12 @@ defmodule PhMicroblog.Jwt do
   def encode(user) do
     %{user_id: user.id, user_name: user.name}
     |> token()
-    |> with_signer(hs256 @secret)
+    |> with_signer(hs256 secret)
     |> sign()
     |> get_compact()
+  end
+
+  defp secret do
+    Application.get_env(:ph_microblog, PhMicroblog.Endpoint)[:secret_key_base]
   end
 end
